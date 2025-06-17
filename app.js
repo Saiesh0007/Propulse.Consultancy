b6b390a764f3911e026a6b4823714e0e0fa8d4e5
Saiesh0007 <07saieshsutar07@gmail.com>
2025-06-17 19:32:24 +0530

    app.js modified

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const path = require("path");
 const contact = require("./routes/contact");
 const newsletter = require("./routes/newsletter");
 const gallery = require("./routes/gallery");
@@ -14,7 +13,6 @@ conn();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
 app.use("/api/v1", contact);
